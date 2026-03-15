f627c842b723321eb71f4977d66607701e398b82
Syed Ahsan Ullah <syedahsanullah.dev@gmail.com>
2026-03-15 05:35:25 +0500

    update2

--- DbOperationsWithEFCoreApp/DbOperationsWithEFCoreApp/Program.cs
+++ DbOperationsWithEFCoreApp/DbOperationsWithEFCoreApp/Program.cs
@@ -1,4 +1,7 @@
 
+using DbOperationsWithEFCoreApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace DbOperationsWithEFCoreApp
 {
     public class Program
@@ -8,6 +11,8 @@ namespace DbOperationsWithEFCoreApp
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to th.
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb"))
+            );
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
